e2e0b06cffacfa30ca37b0e458ecced96814350d
Kwinten Van den Berghe <kwintenvdberghe@gmail.com>
2017-10-30 21:42:59 +0100

    Added input components

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,25 @@
 <template>
-<div>
-	<h1>Test</h1>
-	<input type="text">
+<div class="grid-sm container">
+	<h1>Grab screenshots</h1>
+	<div class="input-group">
+		<input v-model="url" type="text" class="form-input" placeholder="Website URL">
+		<button @click="onSubmit" class="btn btn-primary input-group-btn">Submit</button>
+	</div>
 </div>
 </template>
 
 <script>
 export default {
-	name: "main"
+	name: "main",
+	data() {
+		return {
+			url: ""
+		};
+	},
+	methods: {
+		onSubmit() {
+			console.log(this.url);
+		}
+	}
 };
 </script>
