84fe07561f60affa32de909c7970d0133e9ef5b5
Kwinten Van den Berghe <kwintenvdberghe@gmail.com>
2017-11-02 21:23:33 +0100

    Moved all frontend code to client folder

---
+++ client/src/components/Main.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+	<h1>Grab screenshots</h1>
+	<div class="section-form">
+		<div class="input-group">
+			<input v-model="url" type="text" class="form-input" placeholder="Website URL">
+			<button @click="onSubmit" class="btn btn-primary input-group-btn">Submit</button>
+		</div>
+	</div>
+
+	<div class="columns">
+		<viewport-toggle class="column"/>
+		<viewport-toggle class="column"/>
+		<viewport-toggle class="column"/>
+	</div>
+</div>
+
+</template>
+
+<script>
+import ViewportToggle from "./ViewportToggle";
+
+export default {
+	name: "main",
+	components: {
+		ViewportToggle
+	},
+	data() {
+		return {
+			url: ""
+		};
+	},
+	methods: {
+		onSubmit() {
+			console.log(this.url);
+		}
+	}
+};
+</script>
