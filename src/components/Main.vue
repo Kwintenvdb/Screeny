fbee12cf7e2fbc1a9c7e3318cf55ac1f39340819
Kwinten Van den Berghe <kwintenvdberghe@gmail.com>
2017-11-02 16:57:14 +0100

    Fixed hot reload

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,25 +1,38 @@
 <template>
-<div class="section-form">
+<div>
 	<h1>Grab screenshots</h1>
-	<div class="input-group">
-		<input v-model="url" type="text" class="form-input" placeholder="Website URL">
-		<button @click="onSubmit" class="btn btn-primary input-group-btn">Submit</button>
-	</div>
-
-	<div class="card">
-		<div class="card-header">
-			<div class="card-title h5">Standard</div>
-			<div class="card-subtitle text-gray">1000x1000</div>
+	<div class="section-form">
+		<div class="input-group">
+			<input v-model="url" type="text" class="form-input" placeholder="Website URL">
+			<button @click="onSubmit" class="btn btn-primary input-group-btn">Submit</button>
 		</div>
 	</div>
 
-	<div class="card">
-		<div class="card-header">
-			<div class="card-title h5">Large</div>
-			<div class="card-subtitle text-gray">1920x1080</div>
+	<div class="columns">
+		<div class="card column">
+			<div class="card-header">
+				<div class="card-title h5">Square</div>
+				<div class="card-subtitle text-gray">1000x1000</div>
+			</div>
+		</div>
+
+		<div class="card column">
+			<div class="card-header">
+				<div class="card-title h5">Large</div>
+				<div class="card-subtitle text-gray">1920x1080</div>
+			</div>
+		</div>
+		
+		<div class="card column">
+			<div class="card-header">
+				<div class="card-title h5">Large</div>
+				<div class="card-subtitle text-gray">1920x1080</div>
+			</div>
 		</div>
 	</div>
+	
 </div>
+
 </template>
 
 <script>
