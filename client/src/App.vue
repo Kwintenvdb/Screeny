bfd42c9276c0059f7ea1cb74f74aa6578a88ead8
Kwinten Van den Berghe <kwintenvdberghe@gmail.com>
2017-11-07 17:24:13 +0100

    Some color experiments

--- client/src/App.vue
+++ client/src/App.vue
@@ -26,7 +26,17 @@ export default {
 
 	// background-color: #f6f6f6;
 	text-align: center;
-	background: linear-gradient(to bottom right, #2191FB, #2A2A9E);
+	background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+	// background-image: linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%);
+	// background-image: linear-gradient(-225deg, #20E2D7 0%, #F9FEA5 100%);
+	// background-image: linear-gradient(to top, #50cc7f 0%, #f5d100 100%);
+	// background-image: linear-gradient(-20deg, #fc6076 0%, #ff9a44 100%);
+	// background-image: linear-gradient(to top, #9be15d 0%, #00e3ae 100%);
+	// background-image: linear-gradient(to top, #209cff 0%, #68e0cf 100%);
+	// background-image: linear-gradient(to top, #c471f5 0%, #fa71cd 100%);
+	// background-image: linear-gradient(to top, #37ecba 0%, #72afd3 100%);
+	// background-image: linear-gradient(to right, #4facfe 0%, #00f2fe 100%);
+	// background: linear-gradient(to bottom right, #2191FB, #2A2A9E);
 }
 
 // .page-divider {
